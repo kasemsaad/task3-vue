e6ee4b26d405ef685af0721cbc671128173c09f2
kasem saad <118116111+kasemsaad@users.noreply.github.com>
2023-02-20 02:40:33 +0200

    Done
    Done

---
+++ task3-vue/src/App.vue
@@ -0,0 +1,54 @@
+
+<template>
+  <Nav-Bar1></Nav-Bar1>
+  <Head1></Head1>
+  <section class="pt-4">
+  <div class="container px-lg-5"> 
+       <div class="row gx-lg-5">
+  <Section1 v-for="person in box" :person="person"></Section1>
+</div>
+    </div>
+</section>
+  <Footer1></Footer1>
+</template>
+
+<script>
+import NavBar1 from "./components/NavBar1.vue";
+import Head1 from "./components/Head1.vue";
+import Section1 from "./components/Section1.vue";
+import Footer1 from "./components/Footer1.vue";
+
+export default {
+  data() {
+        return {
+          box:[
+                 { title:"Fresh new layout",
+                   urlimg:            "bi bi-collection"       ,
+                  disc:"With Bootstrap 5, we've created a fresh new layout for this template!",
+},
+{ title:"Free to download",
+                   urlimg:            "bi bi-cloud-download"       ,
+                  disc:"As always, Start Bootstrap has a powerful collectin of free templates.",
+}, { title:"Jumbotron hero header",
+                   urlimg:            "bi bi-card-heading"       ,
+                  disc:"The heroic part of this template is the jumbotron hero header!",
+}, { title:"Feature boxes",
+                   urlimg:            "bi bi-bootstrap"       ,
+                  disc:"We've created some custom feature boxes using Bootstrap icons!",
+}, { title:"Simple clean code",
+                   urlimg:            "bi bi-code"       ,
+                  disc:"We keep our dependencies up to date and squash bugs as they come!",
+}, { title:"A name you trust",
+                   urlimg:            "i bi-patch-check"       ,
+                  disc:"Start Bootstrap has been the leader in free Bootstrap templates since 2013!",
+},
+          ]
+        }
+
+  },
+  components: {
+    NavBar1,Head1,Section1,Footer1,
+  },
+};
+</script>
+
---
+++ task3-vue/src/components/Section1.vue
@@ -0,0 +1,37 @@
+
+<template>
+ 
+    
+            <div class="col-lg-6 col-xxl-4 mb-5">
+                <div class="card bg-light border-0 h-100">
+                    <div class="card-body text-center p-4 flex-column p-lg-5">
+                        <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i :class="person.urlimg"></i></div>
+                        <h2 class="fs-4 fw-bold">{{ person.title }}</h2>
+                        <p class="mb-0">{{ person.disc }}</p>
+                    </div>
+                </div>
+            </div>
+       
+</template>
+<script>
+export default {
+    props: ["person"]
+};
+</script>
+<style>
+.card-body{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.feature{
+    width: 100px;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+i{
+    font-size: 40px;
+}
+</style>
